dbaef5c9783f0b9de9697b7605680c1227c64ec1
Gabriel Mendonça <gmendonca@fingerprint.com.br>
2020-10-09 22:09:59 -0300

    Refatorar app principal e distribuir código
    - Criando serviço para cada nova ação do app
    - Adicionar serviço de filtragem dos repositórios
    - Adicionar serviço de listagem dos repositórios
    - Configurar watch para a property filter
    - atualizar lista flitrada quando input filter é alterado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,30 @@
 (function ($) {
-  
-  /* 
-   * Todo implementar a busca dos membros do github dos maratonistas
-   * API Github - https://api.github.com/
-   **/
+ 
+  $.module('SPA', ['Repository'])
+    .controller('Repositories', ['$scope','RepositoryServiceList', 'RepositoryServiceFilterList', 
+      function ($scope, RepositoryServiceList, RepositoryServiceFilterList) {
+        var controller = this;
 
-  function create_repository (object_data) {
-    return ({
-      id: object_data.id,
-      name: object_data.name,
-      full_name: object_data.full_name,
-      html_url: object_data.html_url,
-      owner: {
-        url: object_data.owner.url,
-        avatar_url: object_data.owner.avatar_url,
-        description: object_data.owner.description
-      }
-    });
-  }
+        this.list = [];
+        this.listFiltered = [];
+        this.filter = "";
 
-  function RepositoriesController ($http) {
-    this.list = [];
-    var controller = this;
+        this.updateFilter = function (filter) {
+          this.filter = filter;
+        }
 
-    function setList (list) {
-      console.log(list);
-      controller.list = list;
-    }
-
-    $http.get('https://api.github.com/orgs/maratonistas/repos').then(function (response) {
-      if (response.status === 200) {
-        var data = response.data;
-        var repositories = [];
-        data.forEach(function (item) {
-          repositories.push(create_repository(item));
+        $scope.$watch('filter', function (filter) {
+          controller.filter = filter ? filter : '';
+          controller.listFiltered = RepositoryServiceFilterList.execute(controller.list, controller.filter);
         });
-        setList(repositories);
-      } else {
-        // não foi possível buscar repositórios
-      }
-    });
-  }
 
-  $.module('SPA', [])
-    .controller('Repositories', RepositoriesController);
+        RepositoryServiceList.execute(function(repositories){
+          controller.list = repositories;
+          controller.listFiltered = controller.list;
+        });
+
+      }
+    ]
+  );
 
 })(angular);
